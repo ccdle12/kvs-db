0142cbcb6521cdd3bc34ef27ba37b83c782e3ae1
ccdle12 <chris.coverdale24@gmail.com>
2019-06-22 21:35:02 +0700

    src/lib.rs: adding args and return vals to fix test errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,19 @@
 pub struct KvStore {}
 
 impl KvStore {
-    pub fn new() {
+    pub fn new() -> KvStore {
+        KvStore {}
+    }
+
+    pub fn set(&self, key: String, val: String) {
         panic!()
     }
 
-    pub fn set() {
+    pub fn get(&self, req: String) -> Option<String> {
         panic!()
     }
 
-    pub fn get() {
-        panic!()
-    }
-
-    pub fn remove() {
+    pub fn remove(&self, key: String) {
         panic!()
     }
 }
