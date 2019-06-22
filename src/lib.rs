b9d5fd0fc7d13d71f848dc8bacc48a0b60f9612a
ccdle12 <chris.coverdale24@gmail.com>
2019-06-22 21:27:05 +0700

    src/lib.rs: blank implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
+pub struct KvStore {}
 
+impl KvStore {
+    pub fn new() {
+        panic!()
+    }
+
+    pub fn set() {
+        panic!()
+    }
+
+    pub fn get() {
+        panic!()
+    }
+
+    pub fn remove() {
+        panic!()
+    }
+}
