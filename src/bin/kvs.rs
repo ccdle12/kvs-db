d04303c1eef6fc89af655fd2b4e9720276ef18c3
ccdle12 <chris.coverdale24@gmail.com>
2019-06-23 00:02:24 +0700

    src/bin/kvs.rs: adding match on get

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -2,7 +2,20 @@
 extern crate clap;
 use clap::App;
 
+use kvs::KvStore;
+
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let m = App::from_yaml(yaml).get_matches();
+
+    // TEMP:
+    let kv_store = KvStore::new();
+
+    // Match on get.
+    if let Some(key) = m.value_of("get") {
+        kv_store.get(key.to_string());
+    }
+
+    // TEMP: just exits on eror for now.
+    std::process::exit(1);
 }
