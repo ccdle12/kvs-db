c78d518b2d2f518a40c2444925a0c7703ee39a2e
ccdle12 <chris.coverdale24@gmail.com>
2019-06-25 23:15:21 +0700

    src/kv.rs: updating return values of the functions

--- project-2/src/kv.rs
+++ project-2/src/kv.rs
@@ -1,3 +1,7 @@
+use crate::Result;
+use std::collections::HashMap;
+use std::path::Path;
+
 /// The `KvStore` stores a key/value pair of strings.
 ///
 /// Key/value pairs are stored in a `HashMap` in memory and not persisted to
@@ -13,33 +17,43 @@
 /// assert_eq!(val, Some("value".to_owned()));
 /// ```
 pub struct KvStore {
-    store: std::collections::HashMap<String, String>,
+    store: HashMap<String, String>,
 }
 
 impl KvStore {
     /// Creates a `KvStore`.
     pub fn new() -> KvStore {
-        let store = std::collections::HashMap::new();
+        let store = HashMap::new();
         KvStore { store }
     }
 
     /// Sets a string value according to a key string.
     ///
     /// If the key already exists, it will overwrite the value.
-    pub fn set(&mut self, key: String, val: String) {
+    pub fn set(&mut self, key: String, val: String) -> Result<()> {
         self.store.insert(key, val);
+        panic!()
     }
 
     /// Gets the string value given a string key.
     ///
     /// Returns None, if the key doesn't exist.
-    pub fn get(&self, key: String) -> Option<String> {
+    pub fn get(&self, key: String) -> Result<Option<String>> {
         // Clone the value from the store.
-        self.store.get(&key).cloned()
+        self.store.get(&key).cloned();
+        panic!()
     }
 
     /// Removes a key/value pair given a string key.
-    pub fn remove(&mut self, key: String) {
+    pub fn remove(&mut self, key: String) -> Result<()> {
         self.store.remove(&key);
+        panic!()
+    }
+
+    /// PLACEHOLDER
+    pub fn open(path: &Path) -> Result<KvStore> {
+        let store = HashMap::new();
+
+        Ok(KvStore { store })
     }
 }
