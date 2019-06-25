615d97043722d166e0e1ac53b3627fe11e0f8e5d
ccdle12 <chris.coverdale24@gmail.com>
2019-06-25 23:15:38 +0700

    src/lib.rs: adding error module and failure extern crates

--- project-2/src/lib.rs
+++ project-2/src/lib.rs
@@ -1,6 +1,12 @@
 #![deny(missing_docs)]
 //! A simple Key/Value store.
 
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
+
+pub use error::{KvStoreError, Result};
 pub use kv::KvStore;
 
+mod error;
 mod kv;
