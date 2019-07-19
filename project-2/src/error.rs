7a8abd7e7a78b255e9cb62ed057bbdef82a178e9
ccdle12 <chris.coverdale24@gmail.com>
2019-07-19 23:23:17 +0100

    src/error.rs: adding error impl for From

--- project-2/src/error.rs
+++ project-2/src/error.rs
@@ -5,6 +5,26 @@ pub enum KvStoreError {
     /// Used for errors that are miscellaneous and/or cannot be explained.
     #[fail(display = "An unknown error has occurred")]
     UnknownError,
+
+    /// Serde serialization and deserialization errors.
+    #[fail(display = "{}", _0)]
+    Serde(#[cause] serde_json::Error),
+
+    /// Standard Input/Output errors.
+    #[fail(display = "{}", _0)]
+    IOError(#[cause] std::io::Error),
+}
+
+impl From<serde_json::Error> for KvStoreError {
+    fn from(err: serde_json::Error) -> KvStoreError {
+        KvStoreError::Serde(err)
+    }
+}
+
+impl From<std::io::Error> for KvStoreError {
+    fn from(err: std::io::Error) -> KvStoreError {
+        KvStoreError::IOError(err)
+    }
 }
 
 /// Shorthand alias for Result in this project, uses the concrete implementation
