3ec7ec0cbaf01a5c3fdd017491c25162ba158732
ccdle12 <chris.coverdale24@gmail.com>
2019-07-19 23:23:45 +0100

    src/kv.rs: big update

--- project-2/src/kv.rs
+++ project-2/src/kv.rs
@@ -1,6 +1,8 @@
 use crate::Result;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use std::path::Path;
+use std::fs::{create_dir_all, File, OpenOptions};
+use std::path::{Path, PathBuf};
 
 /// The `KvStore` stores a key/value pair of strings.
 ///
@@ -13,29 +15,75 @@ use std::path::Path;
 /// # use kvs::KvStore;
 /// let mut store = KvStore::new();
 /// store.set("key".to_owned(), "value".to_owned());
+///
 /// let val = store.get("key".to_owned());
 /// assert_eq!(val, Some("value".to_owned()));
 /// ```
 pub struct KvStore {
+    /// Store is the in memory key/value store.
     store: HashMap<String, String>,
+
+    /// The path to the logs folder, containing the log of events for the DB.
+    path_buf: PathBuf,
 }
 
 impl KvStore {
-    /// Creates a `KvStore`.
-    pub fn new() -> KvStore {
-        let store = HashMap::new();
-        KvStore { store }
-    }
-
-    /// Sets a string value according to a key string.
+    /// Opens a connection to the Key/Value Store via a path to the log folder.
+    /// If the log file don't exist, the file `log.txt` will be created.
     ///
-    /// If the key already exists, it will overwrite the value.
-    pub fn set(&mut self, key: String, val: String) -> Result<()> {
-        self.store.insert(key, val);
-        panic!()
+    /// Example:
+    ///
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// # use fs::env;
+    /// let mut kv_store = KvStore::open(&env::curent_dir()?)?;
+    /// ```
+    pub fn open(path: &Path) -> Result<KvStore> {
+        create_dir_all(&path)?;
+
+        let mut path_buf = PathBuf::from(&path);
+        path_buf.push("log");
+        path_buf.set_extension("txt");
+
+        OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(&path_buf)
+            .expect("failed to create file using path_buf");
+
+        Ok(KvStore {
+            store: HashMap::new(),
+            path_buf,
+        })
     }
 
-    /// Gets the string value given a string key.
+    /// Sets a string value according to a key.
+    /// If the key already exists the value will be overwritten.
+    ///
+    /// Example:
+    ///
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// ...
+    ///
+    /// let key = "hello";
+    /// let value = "world";
+    ///
+    /// let mut kv_store = KvStore::open(log_path)?;
+    /// kv_store.set(key, value)?;
+    /// ```
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {
+        let file_handler = OpenOptions::new()
+            .append(true)
+            .open(&self.path_buf)
+            .expect("failed to open path_buf when setting key/value.");
+
+        serde_json::to_writer(&file_handler, &Command::Set { key, value })?;
+
+        Ok(())
+    }
+
+    /// Retrieves the value of the key/pair given a key as an arguement.
     ///
     /// Returns None, if the key doesn't exist.
     pub fn get(&self, key: String) -> Result<Option<String>> {
@@ -46,14 +94,17 @@ impl KvStore {
 
     /// Removes a key/value pair given a string key.
     pub fn remove(&mut self, key: String) -> Result<()> {
-        self.store.remove(&key);
-        panic!()
-    }
+        let file = File::create("log.txt")?;
+        let remove_cmd = Command::Remove { key };
 
-    /// PLACEHOLDER
-    pub fn open(path: &Path) -> Result<KvStore> {
-        let store = HashMap::new();
-
-        Ok(KvStore { store })
+        serde_json::to_writer(file, &remove_cmd)?;
+        Ok(())
     }
+}
+
+/// PLACEHOLDER
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Command {
+    Set { key: String, value: String },
+    Remove { key: String },
 }
