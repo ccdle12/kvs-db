211900183b86060e170f45663009e715843765f2
Christopher Coverdale <chris.coverdale24@gmail.com>
2019-08-05 09:13:50 +0100

    Project 2 refactor open func (#7)
    * project-2/src/kv: separating open function to sub routines
    
    * project-2/src/kv: adding compact history functionality
    
    * src/kv: moving open file to function call as param

--- project-2/src/kv.rs
+++ project-2/src/kv.rs
@@ -1,6 +1,7 @@
 use crate::{KvStoreError, Result};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
+use std::fs::remove_file;
 use std::fs::{create_dir_all, File, OpenOptions};
 use std::io::prelude::*;
 use std::io::{BufReader, LineWriter};
@@ -32,34 +33,12 @@ impl KvStore {
     /// let mut kv_store = KvStore::open(&current_dir).unwrap();
     /// ```
     pub fn open(path: &Path) -> Result<KvStore> {
-        let mut path_buf = PathBuf::from(path);
-        create_dir_all(&path_buf)?;
+        let path_buf = create_log_file(path)?;
 
-        path_buf.push("log");
-        path_buf.set_extension("txt");
-        let file_handler = OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .open(&path_buf)
-            .expect("failed to create file using path_buf");
+        let store = unpack_log_file(open_file_all_permissions(&path_buf)?)?;
+        let history = compact_history(&store);
 
-        // Create the kv store.
-        let mut store = HashMap::new();
-
-        // Opens the log file and reads each line. The logs are deserialized
-        // and updated in the in-memory store.
-        for line in BufReader::new(file_handler).lines() {
-            let cmd: Command = serde_json::from_str(&line?)?;
-
-            if let Command::Set { key, value } = &cmd {
-                store.insert(key.to_string(), value.to_string());
-            };
-
-            if let Command::Remove { key } = &cmd {
-                store.remove(&key.to_string());
-            };
-        }
+        write_compacted_history(history, &path_buf)?;
 
         Ok(KvStore { store, path_buf })
     }
@@ -81,7 +60,7 @@ impl KvStore {
     /// ```
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
         let set_cmd = Command::Set { key, value };
-        self.write_cmd(&set_cmd, self.log_file_append_only()?)?;
+        write_cmd(&set_cmd, open_file_append_only(&self.path_buf)?)?;
 
         if let Command::Set { key, value } = set_cmd {
             self.store.insert(key, value);
@@ -106,10 +85,10 @@ impl KvStore {
     /// Removes a key/value pair given a string key.
     pub fn remove(&mut self, key: String) -> Result<()> {
         let cmd = Command::Remove { key };
-        self.write_cmd(&cmd, self.log_file_append_only()?)?;
+        write_cmd(&cmd, open_file_append_only(&self.path_buf)?)?;
 
-        if let Command::Remove { key } = cmd {
-            match self.store.remove(&key) {
+        if let Command::Remove { key } = &cmd {
+            match self.store.remove(&key.to_string()) {
                 Some(_x) => return Ok(()),
                 None => return Err(KvStoreError::KeyNotFoundError),
             }
@@ -117,22 +96,99 @@ impl KvStore {
 
         Ok(())
     }
+}
 
-    /// Private helper function to write a command to the log file.
-    fn write_cmd(&self, cmd: &Command, file_handler: impl Write) -> Result<()> {
-        let mut cmd_serialized = serde_json::to_string(&cmd)?;
-        cmd_serialized.push('\n');
+/// Private helper function to write a command to the log file.
+fn write_cmd(cmd: &Command, file_handler: impl Write) -> Result<()> {
+    let mut cmd_serialized = serde_json::to_string(&cmd)?;
+    cmd_serialized.push('\n');
 
-        LineWriter::new(file_handler).write(cmd_serialized.as_bytes())?;
+    LineWriter::new(file_handler).write(cmd_serialized.as_bytes())?;
 
-        Ok(())
+    Ok(())
+}
+
+/// Creates the log file for the `write-ahead-log` given a file path.
+fn create_log_file(path: &Path) -> Result<PathBuf> {
+    let mut path_buf = PathBuf::from(path);
+    create_dir_all(&path_buf)?;
+
+    path_buf.push("log");
+    path_buf.set_extension("txt");
+
+    Ok(path_buf)
+}
+
+/// Opens a file as append only given a path to the file.
+fn open_file_append_only(path: &PathBuf) -> Result<File> {
+    let file_handler = OpenOptions::new().append(true).open(path)?;
+
+    Ok(file_handler)
+}
+
+/// Opens a file with read/write permissions and creates the file it doesn't
+/// exist.
+fn open_file_all_permissions(path: &PathBuf) -> Result<File> {
+    let file_handler = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)?;
+
+    Ok(file_handler)
+}
+
+/// Function that unpacks a log file given a file handler and returns an in-memory
+/// k/v store with the read logs.
+fn unpack_log_file(file_handler: impl Read) -> Result<HashMap<String, String>> {
+    // Create the kv store.
+    let mut store = HashMap::new();
+
+    // Opens the log file and reads each line. The logs are deserialized
+    // and updated in a returned in-memory store.
+    for line in BufReader::new(file_handler).lines() {
+        let cmd: Command = serde_json::from_str(&line?)?;
+
+        if let Command::Set { key, value } = &cmd {
+            store.insert(key.to_string(), value.to_string());
+        };
+
+        if let Command::Remove { key } = &cmd {
+            store.remove(&key.to_string());
+        };
     }
 
-    /// Private helper function to return a file handler as read only to the log.
-    fn log_file_append_only(&self) -> Result<File> {
-        let file_handler = OpenOptions::new().append(true).open(&self.path_buf)?;
-        Ok(file_handler)
+    Ok(store)
+}
+
+/// A helper function that removes redundant entries in the log, returns it as
+/// a vector of commands with the intention of writing it to the log file.
+fn compact_history(store: &HashMap<String, String>) -> Vec<Command> {
+    let mut compacted_history: Vec<Command> = Vec::new();
+
+    for (key, value) in store {
+        let cmd = Command::Set {
+            key: key.to_string(),
+            value: value.to_string(),
+        };
+
+        compacted_history.push(cmd);
     }
+
+    compacted_history
+}
+
+/// A helper function that receives a compacted history and path to the log file.
+/// It removes the logfile and rewrites it with the compacted history.
+fn write_compacted_history(history: Vec<Command>, path: &PathBuf) -> Result<()> {
+    remove_file(path)?;
+    let file_handler = open_file_all_permissions(path)?;
+
+    history
+        .into_iter()
+        .for_each(|x| write_cmd(&x, &file_handler).unwrap());
+
+    Ok(())
 }
 
 /// Command is an enum with each possible command of the database. Each enum
