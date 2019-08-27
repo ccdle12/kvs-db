7db31574b927646ff6f3a764c3eb0da0366c538c
ccdle12 <chris.coverdale24@gmail.com>
2019-08-27 22:11:35 +0100

    bin/kvs-server.rs: adding server cli

---
+++ project-3/src/bin/kvs-server.rs
@@ -0,0 +1,21 @@
+extern crate structopt;
+use kvs::KvsServer;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "kvs-server", about = "The server cli for the kvs.")]
+enum Opt {
+    /// Runs the Server.
+    #[structopt(name = "run")]
+    Run {},
+}
+
+fn main() {
+    match Opt::from_args() {
+        Opt::Run {} => {
+            println!(" [x] Running the KVS Server...");
+            let kvs_server = KvsServer::new();
+            kvs_server.run();
+        }
+    }
+}
