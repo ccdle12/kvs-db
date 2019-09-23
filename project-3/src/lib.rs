63438130ffb595a12f3b7546d37e400284270510
ccdle12 <chris.coverdale24@gmail.com>
2019-09-23 11:38:48 +0100

    src/lib.rs: Including custom error and failure crates

--- project-3/src/lib.rs
+++ project-3/src/lib.rs
@@ -1,7 +1,13 @@
 //! A library for a TCP client and server to run a write-ahead-log kv store.
 
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
+
 pub use client::KvsClient;
+pub use error::{KvStoreError, Result};
 pub use server::KvsServer;
 
 mod client;
+mod error;
 mod server;
