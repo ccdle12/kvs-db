430d7c35593dbb3880fce3c3cdbd00195071c63a
ccdle12 <chris.coverdale24@gmail.com>
2019-09-23 10:14:22 +0100

    src/bin/kvs-server: refactoring to use addr as optional param

--- project-3/src/bin/kvs-server.rs
+++ project-3/src/bin/kvs-server.rs
@@ -2,20 +2,30 @@ extern crate structopt;
 use kvs::KvsServer;
 use structopt::StructOpt;
 
+/// Default listening address for the server - 127.0.0.1:4000.
+const DEFAULT_LISTEN_ADDR: &str = "127.0.0.1:4000";
+
 #[derive(Debug, StructOpt)]
 #[structopt(name = "kvs-server", about = "The server cli for the kvs.")]
 enum Opt {
     /// Runs the Key/Value Store server.
     #[structopt(name = "run")]
-    Run {},
+    Run {
+        #[structopt(
+            help = "The ip address and port as <IP:PORT> the server is serving from, runs the server on default settings if left blank."
+        )]
+        addr: Option<String>,
+    },
 }
 
 fn main() {
     match Opt::from_args() {
-        Opt::Run {} => {
+        Opt::Run { addr } => {
             println!(" [x] Running the KVS Server...");
-            let kvs_server = KvsServer::new();
-            kvs_server.run();
+            match addr {
+                Some(a) => KvsServer::new(a).run(),
+                None => KvsServer::new(String::from(DEFAULT_LISTEN_ADDR)).run(),
+            }
         }
     }
 }
