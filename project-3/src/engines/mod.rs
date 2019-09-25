5dfa4381a87a960885080388f4d8a2c328384f44
ccdle12 <chris.coverdale24@gmail.com>
2019-09-25 10:08:04 +0100

    src/engines: Adding kvs engine from project-2

---
+++ project-3/src/engines/mod.rs
@@ -0,0 +1,29 @@
+//! This module provies the key value storage engines.
+
+use crate::Result;
+
+/// Trait (interface) for the key value storage engine.
+pub trait KvsEngine {
+    /// Sets value of a key - all strings.
+    ///
+    /// If the key already exists then the value will be overwritten.
+    fn set(&mut self, key: String, value: String) -> Result<()>;
+
+    /// Gets the value of a given key.
+    ///
+    /// Returns `None` if the key does not exist.
+    fn get(&mut self, key: String) -> Result<Option<String>>;
+
+    /// Removes a given key.
+    ///
+    /// # Errors
+    ///
+    /// An error `KvsError::KeyNotFound` is returned if a key does not exist.
+    fn remove(&self mut, key: String) -> Result<()>;
+}
+
+mod kvs;
+// mod sled;
+
+pub use self::kvs::KvStore;
+// pub use self::sled::SledKvsEngine;
