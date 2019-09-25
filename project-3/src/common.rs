07ab2d731c9f75680d63ef85a29a540c9b12bdec
ccdle12 <chris.coverdale24@gmail.com>
2019-09-25 10:07:42 +0100

    src/common.rs: Adding file including enum for requests for server

---
+++ project-3/src/common.rs
@@ -0,0 +1,8 @@
+use serde::{Deserialize, Serialize}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum Request {
+    Get { key: String },
+    Set { key: String, value: String },
+    Remove { key: String }
+}
