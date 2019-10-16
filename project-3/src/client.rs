151634f63fbb455f27601e6fcf1439213beb74bf
ccdle12 <chris.coverdale24@gmail.com>
2019-10-16 14:44:34 +0100

    src/client.rs: adding remove

--- project-3/src/client.rs
+++ project-3/src/client.rs
@@ -1,18 +1,21 @@
-use crate::common::{GetResponse, Request, SetResponse};
+use crate::common::{GetResponse, RemoveResponse, Request, SetResponse};
 use crate::{KvStoreError, Result};
 use serde::Deserialize;
 use serde_json::de::{Deserializer, IoRead};
 use std::io::{BufReader, BufWriter, Write};
 use std::net::{TcpStream, ToSocketAddrs};
 
+/// Key Value store client that reads and writes to a Key Value store server.
 pub struct KvsClient {
     reader: Deserializer<IoRead<BufReader<TcpStream>>>,
     writer: BufWriter<TcpStream>,
 }
 
 impl KvsClient {
+    /// Connects to a server given an address.
     pub fn connect<A: ToSocketAddrs>(addr: A) -> Result<Self> {
         let reader = TcpStream::connect(addr)?;
+
         // Creates reference to the same stream but handled independently.
         let writer = reader.try_clone()?;
 
@@ -22,16 +25,13 @@ impl KvsClient {
         })
     }
 
+    /// Sets a key value pair at the server.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        // 1. create a set request given the string.
-        // 2. Serialize the request to the BufWriter using serde since it will
-        //    be in json.
-        // 3. Then we are going to flush, sending the buffer  to the writer.
-        // 4. Read and deserialize the response from the reader.
-        // 5. Send the response back to the client.
         let request = Request::Set { key, value };
+
         serde_json::to_writer(&mut self.writer, &request)?;
         self.writer.flush()?;
+
         let resp = SetResponse::deserialize(&mut self.reader)?;
         match resp {
             SetResponse::Ok(_) => Ok(()),
@@ -39,14 +39,31 @@ impl KvsClient {
         }
     }
 
+    /// Get a value according to a key from the server.
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
         let request = Request::Get { key };
+
         serde_json::to_writer(&mut self.writer, &request)?;
         self.writer.flush()?;
+
         let res = GetResponse::deserialize(&mut self.reader)?;
         match res {
             GetResponse::Ok(r) => Ok(r),
             GetResponse::Err(s) => Err(KvStoreError::StringError(s)),
+        }
+    }
+
+    /// Removes a kv pair.
+    pub fn remove(&mut self, key: String) -> Result<()> {
+        let request = Request::Remove { key };
+
+        serde_json::to_writer(&mut self.writer, &request)?;
+        self.writer.flush()?;
+
+        let res = RemoveResponse::deserialize(&mut self.reader)?;
+        match res {
+            RemoveResponse::Ok(r) => Ok(r),
+            RemoveResponse::Err(e) => Err(KvStoreError::StringError(e)),
         }
     }
 }
