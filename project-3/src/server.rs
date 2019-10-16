d1c3b940fee8efcfd9f0dd474810ff02f5733192
ccdle12 <chris.coverdale24@gmail.com>
2019-10-16 15:25:03 +0100

    src/server.rs: refactoring

--- project-3/src/server.rs
+++ project-3/src/server.rs
@@ -1,14 +1,11 @@
-use crate::common::{GetResponse, Request, SetResponse};
+use crate::common::{GetResponse, RemoveResponse, Request, SetResponse};
 use crate::engines::KvsEngine;
-use crate::error::KvStoreError;
 use crate::Result;
 use serde_json::Deserializer;
 use std::io::prelude::*;
 use std::io::{BufReader, BufWriter};
 use std::net::{TcpListener, TcpStream, ToSocketAddrs};
 
-// TODO(ccdle12): currently using listening address as a string.
-// Maybe should be a custom class or an existing address class.
 /// Server for the Key/Value store.
 pub struct KvsServer<E: KvsEngine> {
     engine: E,
@@ -20,37 +17,26 @@ impl<E: KvsEngine> KvsServer<E> {
     }
 
     pub fn run<A: ToSocketAddrs>(mut self, addr: A) -> Result<()> {
-        let listener = TcpListener::bind(addr)?;
-
-        for stream in listener.incoming() {
-            match stream {
-                Ok(s) => Ok(self.handle_stream(s)?),
-                Err(e) => Err(KvStoreError::IOError(e)),
-            };
-        }
-
-        Ok(())
+        TcpListener::bind(addr)?
+            .incoming()
+            .try_for_each(|s| self.handle_stream(s?))
     }
 
     pub fn handle_stream(&mut self, stream: TcpStream) -> Result<()> {
-        // Reader and Writer buffer for reading tcp stream and writing back over
-        // the stream.
-        let reader = BufReader::new(&stream);
-        let mut writer = BufWriter::new(&stream);
+        let r = BufReader::new(&stream);
+        let mut w = BufWriter::new(&stream);
 
-        // Deserialize the contents of buf reader to a request enum.
-        let request = Deserializer::from_reader(reader).into_iter::<Request>();
+        let request = Deserializer::from_reader(r).into_iter::<Request>();
 
         // Macro for sending reponses back over the tcp stream.
         macro_rules! send_response {
             ($response:expr) => {{
                 let resp = $response;
-                serde_json::to_writer(&mut writer, &resp)?;
-                writer.flush()?;
+                serde_json::to_writer(&mut w, &resp)?;
+                w.flush()?;
             };};
         }
 
-        // Iterates over the serialized requests and handles each request type.
         for req in request {
             let req = req?;
             match req {
@@ -62,7 +48,10 @@ impl<E: KvsEngine> KvsServer<E> {
                     Ok(r) => GetResponse::Ok(r),
                     Err(e) => GetResponse::Err(e.to_string()),
                 }),
-                _ => send_response!(SetResponse::Ok(())),
+                Request::Remove { key } => send_response!(match self.engine.remove(key) {
+                    Ok(_) => RemoveResponse::Ok(()),
+                    Err(e) => RemoveResponse::Err(e.to_string()),
+                }),
             }
         }
 
