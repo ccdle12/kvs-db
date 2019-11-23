7732d5f2175d1f432bcf55f846a0eb38c68122c6
Chris Coverdale <chris.coverdale24@gmail.com>
2019-11-23 11:58:50 +0000

    Adding pool in bin

--- project-4/src/bin/kvs-server.rs
+++ project-4/src/bin/kvs-server.rs
@@ -6,7 +6,7 @@ extern crate log;
 extern crate stderrlog;
 extern crate structopt;
 
-use kvs::{KvStore, KvsEngine, KvsServer, Result, SledKvsEngine};
+use kvs::{KvStore, KvsEngine, KvsServer, NaiveThreadPool, Result, SledKvsEngine, ThreadPool};
 use log::LevelFilter;
 use std::env;
 use std::net::SocketAddr;
@@ -58,15 +58,20 @@ fn main() -> Result<()> {
     info!("kvs-server {}", env!("CARGO_PKG_VERSION"));
     info!("Listening on {}", opt.addr);
 
+    let pool = NaiveThreadPool::new(0)?;
     match opt.engine.unwrap_or(DEFAULT_ENGINE) {
-        Engine::kvs => run_with_engine(KvStore::open(&env::current_dir()?)?, opt.addr),
-        Engine::sled => run_with_engine(SledKvsEngine::new()?, opt.addr),
+        Engine::kvs => run_with_engine(KvStore::open(&env::current_dir()?)?, pool, opt.addr),
+        Engine::sled => run_with_engine(SledKvsEngine::new()?, pool, opt.addr),
     }
 }
 
 /// Internal helper function that runs a KvsServer given the trait KvsEngine
 /// and runs the server. Purely for readability in the main function.
-fn run_with_engine<E: KvsEngine>(engine: E, addr: SocketAddr) -> Result<()> {
-    let server = KvsServer::new(engine);
+fn run_with_engine<E: KvsEngine, P: ThreadPool>(
+    engine: E,
+    pool: P,
+    addr: SocketAddr,
+) -> Result<()> {
+    let server = KvsServer::new(engine, pool);
     server.run(addr)
 }
